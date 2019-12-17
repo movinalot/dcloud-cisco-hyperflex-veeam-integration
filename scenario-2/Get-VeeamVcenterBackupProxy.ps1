f0bd5aec5bf0aa4dfbfba124d7a008d7283b7e47
John McDonough <movinalot@gmail.com>
2019-12-16 19:19:04 -0800

    Initial Commit

---
+++ scenario-2/Get-VeeamVcenterBackupProxy.ps1
@@ -0,0 +1,28 @@
+﻿<#
+.SYNOPSIS
+  Get-VeeamVcenterBackupProxy.ps1 - Get Veeam vCenter Backup Proxy Information
+
+.DESCRIPTION
+  This script PowerShell example of Retrieving Veeam vCenter Backup Proxies.
+
+.NOTES
+  Version:        1.0
+  Author:         John McDonough - jomcdono, Cisco
+  Creation Date:  Dec, 16 2019
+  Purpose: dCloud Cisco HyperFlex with Veeam Integration Lab
+  Scenario: 2
+  
+.EXAMPLE
+  Get-VeeamVcenterBackupProxy
+#>
+[CmdletBinding()]
+param()
+
+Add-PSSnapin -Name VeeamPSSnapin
+
+Connect-VBRServer
+Get-VBRViProxy
+
+Disconnect-VBRServer 
+
+Remove-PSSnapin -Name VeeamPSSnapin
---
+++ scenario-2/Set-VeeamVcenterBackupProxy.ps1
@@ -0,0 +1,62 @@
+﻿<#
+.SYNOPSIS
+  et-VeeamVcenterBackupProxy.ps1 - Set Veeam Backup Proxy
+
+.DESCRIPTION
+  This script is a PowerShell example of Setting a Veeam Backup Proxy
+
+.NOTES
+  Version:        1.0
+  Author:         John McDonough - jomcdono, Cisco
+  Creation Date:  Dec, 16 2019
+  Purpose: dCloud Cisco HyperFlex with Veeam Integration Lab
+  Scenario: 2
+  
+.EXAMPLE
+  Set-VeeamVcenterBackupProxy -ProxyName "VMware Backup Proxy" -TransportMode DirectStorageAccess -ConnectedDatastoreMode Manual -DatastoreName hx-b-ds1 -EnableFailoverToNBD:$false -EnableHostToProxyEncryption:$false -MaxTasks 4
+#>
+[CmdletBinding()]
+param(
+    [Parameter(Mandatory=$true, HelpMessage="Enter a Proxy Name")]
+    [string]$ProxyName,
+
+    [Parameter(Mandatory=$true, HelpMessage="Enter a TransportMode")]
+    [ValidateSet("Auto", "DirectStorageAccess", "HotAdd", "Nbd")]    
+    [string]$TransportMode,
+
+    [Parameter(Mandatory=$true, HelpMessage="Enter a Connected Datastore Mode")]
+    [ValidateSet("Auto", "Manual")] 
+    [string]$ConnectedDatastoreMode,
+
+    [Parameter(Mandatory=$true, HelpMessage="Enter a Datastore Name")]
+    [string]$DatastoreName,
+
+    [Parameter(Mandatory=$true, HelpMessage="Enter a Max Number of Tasks")]
+    [string]$MaxTasks,
+
+    [Parameter(Mandatory=$true)]
+    [switch]$EnableFailoverToNBD,
+
+    [Parameter(Mandatory=$true)]
+    [switch]$EnableHostToProxyEncryption
+
+)
+
+Add-PSSnapin -Name VeeamPSSnapin
+
+Connect-VBRServer
+
+$Datastore = @(Find-VBRViEntity -DatastoresAndVMs -Name $DatastoreName)
+
+$params = @{ 'TransportMode' = $TransportMode;
+             'ConnectedDatastoreMode' = $ConnectedDatastoreMode;
+             'Datastore' = $Datastore;
+             'EnableFailoverToNBD' = $EnableFailoverToNBD;
+             'EnableHostToProxyEncryption' = $EnableHostToProxyEncryption;
+             'MaxTasks' = $MaxTasks }
+
+Get-VBRViProxy -Name $ProxyName | Set-VBRViProxy @params
+
+Disconnect-VBRServer 
+
+Remove-PSSnapin -Name VeeamPSSnapin
